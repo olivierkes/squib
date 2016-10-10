65d981e3ac8ba0048938e2ad1d033f48b5f4c656
Andy Meneely <andy@se.rit.edu>
2016-10-10 00:13:24 -0400

    Goofing around
    [skip ci]

--- lib/squib/api/background.rb
+++ lib/squib/api/background.rb
@@ -6,9 +6,11 @@ module Squib
 
     # DSL method. See http://squib.readthedocs.io
     def background(opts = {})
+      dsl_defaults = {} # nothing non-standard for this method
       range = Args::CardRange.new(opts[:range], deck_size: size)
-      draw  = Args::Draw.new(custom_colors).load!(opts, expand_by: size, layout: layout, dpi: dpi)
-      range.each { |i| @cards[i].background(draw.color[i]) }
+      draws = Args::Builder.new.build(self, opts, Args::Draw2, dsl_defaults)
+      # draw  = Args::Draw.new(custom_colors).load!(opts, expand_by: size, layout: layout, dpi: dpi)
+      range.each { |i| @cards[i].background(draws[i]) }
     end
 
   end
---
+++ lib/squib/args/builder.rb
@@ -0,0 +1,6 @@
+module Squib
+  module Args
+    class Builder
+    end
+  end
+end
